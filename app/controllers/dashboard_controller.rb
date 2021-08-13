84536ef667b074752a3d88637aea21a0b0b556fa
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-08-13 18:36:57 -0500

    add faker

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 
 class DashboardController < ApplicationController
   def index
+    @recent_products= Product.three_most_recent
+    @us_location = Product.us_location  
+    @most_reviews = Product.most_reviews
   end
 end
